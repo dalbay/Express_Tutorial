7478ba678a9d503631b92f959b0e9d32e55dbbf5
dalbay <fadalbay@gmail.com>
2019-10-16 13:50:07 -0400

    refactor routing

--- README.md
+++ README.md
@@ -290,4 +290,99 @@ app.delete('/api/v1/tours/:id', (request, response) => {
   });
 });
 ```  
-Run the server and make a DELETE request in Postman with a value of 5  ```127.0.0.1:3000/api/v1/tours/5```; The output in postman is no content at all.
+Run the server and make a DELETE request in Postman with a value of 5  ```127.0.0.1:3000/api/v1/tours/5```; The output in postman is no content at all.  
+<br/>
+
+## Refactoring Our Routes
+- reorganize so that the routes are together and the handler functions are also together.
+- export the handler functions into their own functions. 
+```JavaScript
+// CRUD Functions All Together:
+
+// GET ALL Tours functions:
+const getAllTours = (request, response) => {
+  response.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
+  });
+};
+
+// GET A Tour function:
+const getTour = (request, response) => {
+  console.log(request.params);
+
+  const id = request.params.id * 1;
+
+  if (id > tours.length) {
+    return response.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+  const tour = tours.find(element => element.id === id);
+  response.status(200).json({
+    status: 'success',
+    data: {
+      tour
+    }
+  });
+};
+
+// CREATE a Tour function:
+const createTour = (request, response) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId, price: 230 }, request.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      response.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      });
+    }
+  );
+};
+
+// UPDATE a Tour function
+const updateTour = (request, response) => {
+  if (request.params.id * 1 > tours.length) {
+    response.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+  response.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>'
+    }
+  });
+};
+
+// DELETE a Tour function
+const deleteTour = (request, response) => {
+  if (request.params.id * 1 > tours.length) {
+    response.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+  response.status(204).json({
+    status: 'success',
+    data: null
+  });
+};
+
+// use route() method with the URL and attach HTML methods with the same route.
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).post(deleteTour);
+```
--- app.js
+++ app.js
@@ -11,43 +11,34 @@ const app = express();
 app.use(express.json());
 
 // Read Data (tours) - an array of JSON objects inside the dev-data folder.
-// We don't have to read data in the route handler, Read data in top-level code; it will be executed only once right after the application startup.
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-// Define a Route to get ALL Tours:
-app.get('/api/v1/tours', (request, response) => {
-  // the callback function is usually called 'The Route Handler';
-  // only callback functions will run inside the Event Loop. In here we cannot have any blocking code!
-  // read the data before we can send it to the client(in top-level code).
-  // send it to the client:
+
+// get ALL Tours functions:
+const getAllTours = (request, response) => {
   response.status(200).json({
-    // we want to send back JSend standard format:
     status: 'success',
-    results: tours.length, // this only makes sens when we sending multiple objects(array)
+    results: tours.length,
     data: {
       tours
     }
   });
-});
+};
 
-// Define a Route to GET ONE Tour by defining a variable:
-app.get('/api/v1/tours/:id', (request, response) => {
+// get a Tour function:
+const getTour = (request, response) => {
   console.log(request.params);
 
-  const id = request.params.id * 1; // converts string to number.
+  const id = request.params.id * 1;
 
   if (id > tours.length) {
     return response.status(404).json({
       status: 'fail',
       message: 'Invalid ID'
-    }); // exit the function
+    });
   }
-  // we could also try to get the tour first,
-  // and then test and see if we got a tour:
-  // if(!tour){
-
   const tour = tours.find(element => element.id === id);
   response.status(200).json({
     status: 'success',
@@ -55,15 +46,13 @@ app.get('/api/v1/tours/:id', (request, response) => {
       tour
     }
   });
-});
+};
 
-// Define a new Route to add a new Tour with the HTTP POST Request
-app.post('/api/v1/tours', (request, response) => {
-  // the id of an object is handled by the db - takes id of last object and adds 1
+// create a Tour function:
+const createTour = (request, response) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId, price: 230 }, request.body);
 
-  // push the new tour to the tours array
   tours.push(newTour);
   fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
@@ -77,10 +66,10 @@ app.post('/api/v1/tours', (request, response) => {
       });
     }
   );
-});
+};
 
-// Update data with patch - properties on the object
-app.patch('/api/v1/tours/:id', (request, response) => {
+// update a Tour function
+const updateTour = (request, response) => {
   if (request.params.id * 1 > tours.length) {
     response.status(404).json({
       status: 'fail',
@@ -93,10 +82,10 @@ app.patch('/api/v1/tours/:id', (request, response) => {
       tour: '<Updated tour here...>'
     }
   });
-});
+};
 
-// Delete data
-app.delete('/api/v1/tours/:id', (request, response) => {
+// delete a Tour function
+const deleteTour = (request, response) => {
   if (request.params.id * 1 > tours.length) {
     response.status(404).json({
       status: 'fail',
@@ -107,18 +96,29 @@ app.delete('/api/v1/tours/:id', (request, response) => {
     status: 'success',
     data: null
   });
-});
+};
+
+// Define a Route to get ALL Tours:
+//app.get('/api/v1/tours', getAllTours);
+
+// Define a Route to GET ONE Tour
+//app.get('/api/v1/tours/:id', getTour);
+
+// Define a new Route to add a new Tour
+app.post('/api/v1/tours', createTour);
+
+// Update a Tour
+//app.patch('/api/v1/tours/:id', updateTour);
+
+// Delete data
+//app.delete('/api/v1/tours/:id', deleteTour);
+
+// use the route() method with the URL and attach HTML methods with the same route.
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).post(deleteTour);
 
 const port = 3000;
 // use the listen method to create a server; pass in a port and a callback function that will be called as soon as the server starts listening.
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
-
-// // when someone hits that URL with a get request, what we want to happen is specified in a callback function as the second argument,
-// // and with the callback function we pass along a request and response argument.
-// app.get('/', (request, response) => {
-//   response
-//     .status(200)
-//     .json({ message: 'Hellow from the server side!', app: 'Natours' });
-// });
